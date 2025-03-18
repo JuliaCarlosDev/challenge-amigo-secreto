3512dc8c70829bf7bff635ef16aaaaf391431ef3
JuliaCarlosDev <nicothedarkk@gmail.com>
2025-03-18 20:27:46 -0300

    add-nvs-nomes

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = []; // Lista de amigos
 
 function adicionarAmigo() {
-// Pega o nome do campo, tira espaços
+    // Pega o nome do campo, tira espaços
     let nomeInput = document.getElementById('amigo');
     let nome = nomeInput.value.trim();
 
-// Se vazio, avisa e sai
+    // Se vazio, avisa e sai
     if (nome === '') {
         alert('Por favor, insira um nome.');
         return;
     }
 
-// Adiciona na lista e limpa o campo
+    // Adiciona na lista e limpa o campo
     amigos.push(nome);
     nomeInput.value = '';
 
     let listaAmigos = document.getElementById('listaAmigos');
-    listaAmigos.innerHTML = ""; 
-// Limpa a lista
+    listaAmigos.innerHTML = ""; // Limpa a lista
 
+    let listaHTML = "";
     for (let i = 0; i < amigos.length; i++) {
-        let itemLista = document.createElement('li');
-        itemLista.textContent = amigos[i];
-        listaAmigos.appendChild(itemLista);
+        listaHTML += `<li>${amigos[i]}</li>`;
     }
+    listaAmigos.innerHTML = listaHTML;
 }
